41baed58bc7d7c132812a5fff44aca2f076212a8
Arshya Soleimani <arrshya@gmail.com>
2023-05-23 22:37:44 +0000

    implemented x and y axis

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
-// Reads the data and creates barebones bars
-// Numeric values will be displayed and the position specified by user
-const drawBarChart = function (data, options, element) {
+$(document).ready(function(){
+
+  // function that creates the axis of the chart
+  const createAxis = function (options,element) {
+
+    // create div for vertical line and set height
+    const vLine = $('<div>').addClass('v_line')
+    vLine.css('height', options.chartHeight);
+    $(element).append(vLine);
+
+    // create div for horizontal line and set width
+    const hLine = $('<div>').addClass('h_line')
+    hLine.css('width', options.chartWidth);
+    $(element).append(hLine);
+  }
+
+// function that creates the bars
+const createBars = function () {
   return;
 }
 
 
-// Sets bar colors
-const barColor = function (color) {
-  return;
-}
 
-// Sets label colors
-const labelColor = function (color) {
-  return;
-}
 
-// Sets bar spacing
-const barSpacing = function (spacing) {
-  return;
-}
 
-// Sets x-axis lables
-const xAxis = function () {
-  return;
-}
+  const drawBarChart = function (data, options, element) {
+    // rendering chart into user defined element
+    const chartBody = $('<div>').addClass('chart_body');
+    $(element).append(chartBody);
 
-// Sets y-axis values (make user specify the spacing between ticks in 'options')
-const yAxis = function (options) {
-  return;
-}
+    createAxis(options, '.chart_body');
+  }
 
-// Sets title
-const setTitle = function (options) {
-  return;
-}
+
+
+
+
+  // Test Code:
+  let element = '#reference';
+  let data = [1,2,3,4,5];
+  let options = {chartWidth:200, chartHeight:200};
+
+  drawBarChart(data,options,element);
+
+});
+
+
+
 
