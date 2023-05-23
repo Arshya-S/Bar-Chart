f9ef911dc85a0222bb64941934a46045f202ea2e
Arshya Soleimani <arrshya@gmail.com>
2023-05-23 02:00:04 +0000

    general structure of the project

---
+++ main.js
@@ -0,0 +1,37 @@
+// Reads the data and creates barebones bars
+// Numeric values will be displayed and the position specified by user
+const drawBarChart = function (data, options, element) {
+  return;
+}
+
+
+// Sets bar colors
+const barColor = function (color) {
+  return;
+}
+
+// Sets label colors
+const labelColor = function (color) {
+  return;
+}
+
+// Sets bar spacing
+const barSpacing = function (spacing) {
+  return;
+}
+
+// Sets x-axis lables
+const xAxis = function () {
+  return;
+}
+
+// Sets y-axis values (make user specify the spacing between ticks in 'options')
+const yAxis = function (options) {
+  return;
+}
+
+// Sets title
+const setTitle = function (options) {
+  return;
+}
+
