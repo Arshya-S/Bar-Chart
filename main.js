99f87bfbc81c24c2a081a617df5e74be040fb498
Arshya Soleimani <arrshya@gmail.com>
2023-05-24 19:28:49 +0000

    added the vertical lines for the bars

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     $(element).append(vLine);
 
     // create div for horizontal line and set width
-    const hLine = $('<div>').addClass('h_line')
+    const hLine = $('<div>').addClass('h_line');
     hLine.css('width', options.chartWidth);
     $(element).append(hLine);
 
@@ -19,19 +19,33 @@ $(document).ready(function(){
       let tick = $('<div>').addClass('tick').css('top', i * interval + 'px').css('left', '-7.5px');
       $(vLine).append(tick);
     }
+
   }
 
   // function that creates the bars
-  const createBars = function (data,element,options) {
+  const createBars = function (data,options,element) {
+    // finds the max value of the data
+    let maxValue = Math.max(...data);
+    // partitions x-axis to create evenly spread out bars
+    let division = options.chartWidth / data.length;
 
-    for (let value of data) {
+    // loop to add the left and right vertical lines of the bars
+    // height of bar is determined by bar chart height
+    // position is determined by the partitioning done by 'division'
+    for (let i = 0; i < data.length; i++) {
       let leftVertical = $('<div>').addClass('bar');
-      leftVertical.css('height', maxValue)
+      leftVertical.css('height', (data[i]/maxValue)*options.chartHeight + 'px');
+      leftVertical.css('left', 20 + (i * division));
 
+      let rightVertical = $('<div>').addClass('bar');
+      rightVertical.css('height', (data[i]/maxValue)*options.chartHeight + 'px');
+      rightVertical.css('left', 40 + (i * division));
+
+
+
+      $(element).append(leftVertical)
+      $(element).append(rightVertical)
     }
-
-
-
   }
 
 
@@ -44,6 +58,8 @@ $(document).ready(function(){
     $(element).append(chartBody);
 
     createAxis(options, '.chart_body');
+
+    createBars(data,options,'.h_line');
   }
 
 
@@ -52,7 +68,7 @@ $(document).ready(function(){
 
   // Test Code:
   let element = '#reference';
-  let data = [2,4,5,6,9];
+  let data = [6,4,5,6,9];
   let options = {chartWidth:500, chartHeight:500, tickCount: 5};
 
   drawBarChart(data,options,element);
